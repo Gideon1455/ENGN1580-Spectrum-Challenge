cb712201c0523ade57a7522fdab7116146c31751
mstephe7 <matthew_stephens@brown.edu>
2023-05-14 14:21:50 -0400

    -update

--- components/receiver_example.py
+++ components/receiver_example.py
@@ -4,10 +4,11 @@ from os import getcwd
 path.append(getcwd() + "/components")
 path.append(getcwd() + "/utils")
 
-from channel import Channel # type: ignore
-from receiver import Receiver # type: ignore
-from transmitter import Transmitter # type: ignore
+from channel import Channel  # type: ignore
+from receiver import Receiver  # type: ignore
+from transmitter import Transmitter  # type: ignore
 from utils import dec16_to_hex16, hex16_to_dec16
+
 
 def start(cid, uid):
     channel = Channel(cid, uid)
@@ -36,11 +37,11 @@ def start(cid, uid):
             if current_frame not in seen_frames:
                 bits = ''
                 for i in range(0, len(signal), 4):
-                    bits += '0' if hex16_to_dec16(signal[i:i+4]) < 0 else '1'
+                    bits += '0' if hex16_to_dec16(signal[i:i + 4]) < 0 else '1'
                 print(bits)
                 b_hat = ''
                 for i in range(0, len(bits), 4):
-                    b_hat += hex(int(bits[i:i+4], 2)).split('x')[-1]
+                    b_hat += hex(int(bits[i:i + 4], 2)).split('x')[-1]
 
                 receiver.post_b_hat(b_hat)
                 print(f'received frame {current_frame}')
@@ -50,6 +51,7 @@ def start(cid, uid):
             _, sent, errors = channel.get_score()
             print(f'Total sent: {sent}, Total errors: {errors}')
             break
+
 
 if __name__ == '__main__':
     CID = 'JustinTest'
--- components/transmitter_example.py
+++ components/transmitter_example.py
@@ -4,10 +4,11 @@ from os import getcwd
 path.append(getcwd() + "/components")
 path.append(getcwd() + "/utils")
 
-from channel import Channel # type: ignore
-from receiver import Receiver # type: ignore
-from transmitter import Transmitter # type: ignore
+from channel import Channel  # type: ignore
+from receiver import Receiver  # type: ignore
+from transmitter import Transmitter  # type: ignore
 from utils import dec16_to_hex16, hex16_to_dec16
+
 
 def start(cid, uid):
     channel = Channel(cid, uid)
@@ -35,14 +36,14 @@ def start(cid, uid):
                 continue
 
             if current_frame not in sent_frames:
-                b = transmitter.get_b(samples_per_frame / 8) # 8 bits, 2 hex chars
+                b = transmitter.get_b(samples_per_frame / 8)  # 8 bits, 2 hex chars
                 bits = bin(int(b, 16)).split('b')[-1]
                 bits = '0' * (128 - len(bits)) + bits
 
                 to_send = ''
                 for bit in bits:
                     to_send += '7FFF' if bit == '0' else '8001'
-                
+
                 transmitter.transmit(current_frame, to_send)
                 print(f'sending frame {current_frame}')
                 sent_frames.append(current_frame)
@@ -51,6 +52,7 @@ def start(cid, uid):
             _, sent, errors = channel.get_score()
             print(f'Total sent: {sent}, Total errors: {errors}')
             break
+
 
 if __name__ == '__main__':
     CID = 'JustinTest'
