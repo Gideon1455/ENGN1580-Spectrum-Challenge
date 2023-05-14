75cc8100682b8bf9c43b54a6e56c07d6be0b22b4
mstephe7 <matthew_stephens@brown.edu>
2023-05-14 14:52:08 -0400

    update the random num generator

--- components/pseudorandom_generator.py
+++ components/pseudorandom_generator.py
@@ -1,16 +1,18 @@
 def generate_codeword(entry):
     entry = str(bin(entry))
-    entry = "0000" + entry[2:]
+    entry = "000" + entry[2:]
     entry_array = []
     for el in entry: entry_array.append(el)
+    print(entry_array)
     codeword = []
     for i in range(256):
-        thirty_first_bit = int(entry_array[31])
-        twenty_eighth_bit = int(entry_array[28])
+        thirty_first_bit = int(entry_array[30])
+        twenty_eighth_bit = int(entry_array[27])
         codeword.append(thirty_first_bit)
         xor = twenty_eighth_bit ^ thirty_first_bit
         entry_array.pop()
         entry_array.insert(0, str(xor))
     print(codeword)
+
 
 generate_codeword(140256304)
